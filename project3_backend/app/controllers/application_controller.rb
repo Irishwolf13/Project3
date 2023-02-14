3cac828ed996ea6e033bab42e7f25f206c0807d4
John Rooney <rooneyjohn@gmail.com>
2023-02-14 10:25:41 -0500

    started working on the back end

--- project3_backend/app/controllers/application_controller.rb
+++ project3_backend/app/controllers/application_controller.rb
@@ -6,4 +6,28 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/users/:id/solutions" do
+    { message: "Return a list of all solutions posted by user"}.to_json
+  end
+
+  get "/solutions/:id" do
+    { message: "Returns individual solution"}.to_json
+  end
+
+  get "/problems/:id" do
+    { message: "Returns an individual problem"}.to_json
+  end
+
+  get "/problems/:id/comments" do
+    { message: "Returns all comments for individual problem"}.to_json
+  end
+
+  get "/problems/:id/likes" do
+    { message: "Returns number of likes for individual problem"}.to_json
+  end
+
+  get "/comments/:id" do
+    { message: "Returns individual comment_content"}.to_json
+  end
+
 end
