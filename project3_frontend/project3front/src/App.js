15baa8026cea6f3bb38fa521ffa47b660f42701d
Tatiana <Bowmantatiana2@gmail.com>
2023-02-14 07:24:10 -0800

    nav-bar

--- project3_frontend/project3front/src/App.js
+++ project3_frontend/project3front/src/App.js
@@ -1,15 +1,19 @@
 import './App.css';
-import React, { useState, useEffect } from 'react';
-import { Routes, Route } from "react-router-dom"
+// import React, { useState, useEffect } from 'react';
+import React from 'react';
+//import { Routes, Route } from "react-router-dom"
+import NavigationBar from "./components/NavigationBar"
 
 function App() {
+
   return (
     <div className="App">
-      <Routes>
+     <NavigationBar /> 
+      {/* <Routes>
         <Route path="/"/>
         <Route path="/user"/>
         <Route path="/problem"/>
-      </Routes>
+      </Routes> */}
     </div>
   );
 }
--- project3_frontend/project3front/src/components/NavigationBar.js
+++ project3_frontend/project3front/src/components/NavigationBar.js
@@ -1,7 +1,36 @@
-import React from "react"
+import React, {useState} from "react"
+import Login from "./Login"
+// import User from "./User"
+ import SearchMenu from "./SearchMenu"
+
 
 function NavigationBar(){
-    
+    const [loggingIn, setLoggingIn] = useState(false)
+    const [searchToggle, setSearchToggle] = useState(false)
+
+    function handleLoggingIn(){
+        setLoggingIn(!loggingIn)
+    }
+
+    function handleSearchToggle(){
+        setSearchToggle(!searchToggle)
+    }
+    let user = "user placeholder"
+    return (
+        <div className="nav-bar">
+         <div> {user} / <button onClick={handleLoggingIn}>{loggingIn ? "logout" : "login"}</button></div>
+         {loggingIn ? <Login/> : null}
+
+         <div id="solves">
+         <button>Solves</button>
+         </div>
+
+        <div id="search">
+          <button onClick={handleSearchToggle}>Search</button>
+          {searchToggle ? <SearchMenu/> : null}
+        </div>
+        </div>
+    )
 }
 
 export default NavigationBar
