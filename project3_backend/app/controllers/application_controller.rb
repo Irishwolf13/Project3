8b8030f341e2c5bccf80754b8c0b1752c6abd5cd
John Rooney <rooneyjohn@gmail.com>
2023-02-14 14:44:36 -0500

    got gets working

--- project3_backend/app/controllers/application_controller.rb
+++ project3_backend/app/controllers/application_controller.rb
@@ -1,33 +1,29 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+
+  get "/users/:id" do
+    User.find(params[:id]).to_json
   end
 
   get "/users/:id/solutions" do
-    { message: "Return a list of all solutions posted by user"}.to_json
+    User.find(params[:id]).solutions.to_json(include: :problem)
   end
 
   get "/solutions/:id" do
-    { message: "Returns individual solution"}.to_json
+    Solution.find(params[:id]).to_json(include: {user: {only: [:user_name]}})
   end
 
   get "/problems/:id" do
-    { message: "Returns an individual problem"}.to_json
+    myProblem = Problem.find(params[:id])
+    # myProblem.to_json
+    # myProblem.to_json(include: :solutions)
+    # myProblem.to_json(include: {solutions: {include: {user:{only: [:user_name]}}}})
+    myProblem.to_json(include: {solutions: {include: :user}})
   end
 
-  get "/problems/:id/comments" do
-    { message: "Returns all comments for individual problem"}.to_json
-  end
-
-  get "/problems/:id/likes" do
-    { message: "Returns number of likes for individual problem"}.to_json
-  end
-
-  get "/comments/:id" do
-    { message: "Returns individual comment_content"}.to_json
+  get "/problems/:id/solutions" do
+    myProblem = Problem.find(params[:id])
+    myProblem.to_json(include: {solutions: {include: :comments}})
   end
 
 end
