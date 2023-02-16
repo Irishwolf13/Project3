e92a3622e0382ff27b9c51145c87fd2223dfdaa0
John Rooney <rooneyjohn@gmail.com>
2023-02-16 10:42:29 -0500

    Got routers working!

--- project3_frontend/project3front/src/components/SolutionCard.js
+++ project3_frontend/project3front/src/components/SolutionCard.js
@@ -5,57 +5,53 @@ import like_image from "../assets/like-button.png"
 import SubmitComment from "./SubmitComment"
 
 function SolutionCard({ id, likes, solve, language, comments, problem }) {
-    const [myLikes, setLikes]= useState(likes)
-    const [myComments, setMyComments] = useState([])
-     // tried to add comment function here and didn't work
-     useEffect(() => {
-       setMyComments(comments)
-     }, [])
-     
-    console.log(myComments)
-     const addComment = (newComment) => {
-      console.log(newComment)
-        setMyComments([...comments, newComment])
-     }
+  const [myLikes, setLikes]= useState(likes)
+  const [myComments, setMyComments] = useState([])
 
-    let displayComments = myComments.map(item =>
-      <CommentCard
-        key={item.id}
-        commentText={item.comment}
-      />
-    )
+  useEffect(() => {
+    setMyComments(comments)
+  }, [])
 
+  const addComment = (newComment) => {
+    setMyComments([...comments, newComment])
+  }
 
+  let displayComments = myComments.map(item =>
+    <CommentCard
+      key={item.id}
+      commentText={item.comment}
+    />
+  )
 
-    const handleLike = (e) => {
-      fetch(`http://localhost:9292/solutions/${id}/likes`, {
-        method: 'PATCH', headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({num_of_likes: myLikes+1})
-      })
-      .then(setLikes(prev => prev+1))
-    }
+  const handleLike = (e) => {
+    fetch(`http://localhost:9292/solutions/${id}/likes`, {
+      method: 'PATCH', headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify({num_of_likes: myLikes+1})
+    })
+    .then(setLikes(prev => prev+1))
+  }
 
-    return (
-      <>
-        <div className="mx-auto w-4/6">
-          <div className="solution-card-outer">
-            <div className="absolute inline-block bg-[#0000a8] w-[99%] h-8 top-1">
-              <p className="inline-block font-win95bold text-white pl-1">Placeholder Name</p>
-              <div className="relative float-right top-1 right-1">
-                <CustomButton buttonImage={like_image} onClick={handleLike}/>
-              </div>
-            </div>
-            <div className="solution-card-inner">
-              <p className="font-win95bold">Language: {language}</p>
-              <h4 className="font-win95">{solve}</h4>
-              <p className="font-win95bold"> This solution has {myLikes} likes</p>
+  return (
+    <>
+      <div className="mx-auto w-4/6">
+        <div className="solution-card-outer">
+          <div className="absolute inline-block bg-[#0000a8] w-[99%] h-8 top-1">
+            <p className="inline-block font-win95bold text-white pl-1">Solution #{id}</p>
+            <div className="relative float-right top-1 right-1">
+              <CustomButton buttonImage={like_image} onClick={handleLike}/>
             </div>
           </div>
-          {displayComments}
-          <SubmitComment id={id} addComment={addComment} />
+          <div className="solution-card-inner">
+            <p className="font-win95bold">Language: {language}</p>
+            <h4 className="font-win95">{solve}</h4>
+            <p className="font-win95bold"> This solution has {myLikes} likes</p>
+          </div>
         </div>
-      </>
-    )
+        {displayComments}
+        <SubmitComment id={id} addComment={addComment} />
+      </div>
+    </>
+  )
 }
 
 export default SolutionCard
